0dcda4f58a8649decce184b724f30b16b4c2f43d
skie1997 <skie1997@outlook.com>
2021-11-18 17:40:28 +0800

    mobile style response

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -38,7 +38,11 @@ export default function HomePage() {
         console.log("value", value)
         i18n.changeLanguage(value);
     }
-      
+
+    console.log('iii', i18n.defaultValue)
+
+
+    
 
     // render() {
         
@@ -71,6 +75,7 @@ export default function HomePage() {
                 <Select defaultValue={"en"} onChange={handleChange}>
                     <Option value="zh">中文</Option>
                     <Option value="en">EN</Option>
+                    
                 </Select>
                 
             </Navbar>
@@ -94,8 +99,8 @@ export default function HomePage() {
                     </Row>
                     
                 </Container>
-                <Row>
-                    <Image src={banner1_bk} />
+                <Row style={{marginRight: 0}}>
+                    <Image src={banner1_bk} style={{padding: 0, marginRight: 0}}/>
                     {/* <p className="section01-bottom">C A L L I O P E </p> */}
                 </Row>              
             </div>
@@ -129,7 +134,7 @@ export default function HomePage() {
                         </div> 
                     </Row>
                     <Row className="section02-row02">
-                        <Col xxl={4} xl={4} lg={4} md={4} sm={6} xs={6}>
+                        <Col xxl={4} xl={4} lg={4} md={4} sm={6} xs={12} className="section02-row02-col" style={{paddingLeft: 0}}>
                             <div className="card02">
                                 <img className = "card02-icon" src={enterprise} alt="enterprise"></img>
                                 <h2 className = "card02-title">{t('enterprise')}</h2>
@@ -137,7 +142,7 @@ export default function HomePage() {
                                 <a class="btn btn-lg btn-primary section02-btn" href="../../components/#navbar" role="button">{t('enterprise-explore')}</a>
                             </div>
                         </Col>
-                        <Col xxl={4} xl={4} lg={4} md={4} sm={6} xs={6}>
+                        <Col xxl={4} xl={4} lg={4} md={4} sm={6} xs={12} className="section02-row02-col">
                             <div className="card02">
                                 <img className = "card02-icon" src={software} alt="software"></img>
                                 <h2 className = "card02-title">{t('software')}</h2>
@@ -145,7 +150,7 @@ export default function HomePage() {
                                 <a class="btn btn-lg btn-primary section02-btn" href="../../components/#navbar" role="button">{t('software-explore')}</a>
                             </div>
                         </Col>
-                        <Col xxl={4} xl={4} lg={4} md={4} sm={6} xs={6}>
+                        <Col xxl={4} xl={4} lg={4} md={4} sm={6} xs={12} className="section02-row02-col" style={{paddingRight: 0}}>
                             <div className="card02">
                                 <img className = "card02-icon" src={users} alt="users"></img>
                                 <h2 className = "card02-title">{t('users')}</h2>
